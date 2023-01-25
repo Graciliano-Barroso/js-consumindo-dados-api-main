4e234194580c3bf754fd850b213de16b7828b370
Graciliano-Barroso <grico484@gmail.com>
2023-01-25 16:19:33 -0400

    feat: Evitando problemas

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultaCEP.json();
@@ -14,10 +16,11 @@ async function buscaEndereco(cep) {
         logradouro.value = consultaCEPConvertida.logradouro;
         bairro.value = consultaCEPConvertida.bairro;
         estado.value = consultaCEPConvertida.uf;
-        
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
         console.log(erro);
     }
 }
